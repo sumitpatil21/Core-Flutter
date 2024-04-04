2f4b183c4f2457ab6fe0b2162482a46c04826290
Sumit Patil <148967002+sumitpatil21@users.noreply.github.com>
2024-04-04 22:24:37 +0530

    Update 1.dart

--- OOPs/1.dart
+++ OOPs/1.dart
@@ -1,1 +1,25 @@
+import 'dart:io';
+
+class Add {
+  int addition({required List<int> a}) {
+    int sum = 0;
+    for (int i = 0; i < a.length; i++) {
+      sum = sum + a[i];
+    }
+    return sum;
+  }
+}
+
+void main() {
+  int n;
+  stdout.write("Enter a Size :");
+  n = int.parse(stdin.readLineSync()!);
+  List<int> a = [];
+  for (int i = 0; i < n; i++) {
+    stdout.write("A[$i]");
+    a.add(int.parse(stdin.readLineSync()!));
+  }
+  Add a1 = Add();
+  print("Sum is : ${a1.addition(a: a)}");
+}
 
